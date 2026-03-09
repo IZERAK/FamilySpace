f3fc2c98df3f8efd8189331631719cc41591b216
Андрей Калюжный QA <a.kaliyzhniy@megapolis-it.ru>
2026-03-10 00:36:18 +0300

    минимальные изменения

--- src/main.tsx
+++ src/main.tsx
@@ -8,7 +8,8 @@ import Home from "./pages/Home.tsx";
 createRoot(document.getElementById("root")!).render(
   <BrowserRouter>
     <Routes>
-      <Route path="/auth" element={<Auth />} />
+      <Route index element={<Auth />} />
+      <Route index path="/auth" element={<Auth />} />
       <Route path="/reg" element={<Reg />} />
       <Route path="/home" element={<Home />}></Route>
     </Routes>
